73aed7685953536039834b6bfea31a5c5fe30623
Samich80 <samich80@mail.ru>
2021-01-11 11:46:50 +0300

    add spider bravebird

---
+++ web_crawler/spiders/bravebird.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class BravebirdSpider(scrapy.Spider):
+    name = 'bravebird'
+    allowed_domains = ['bravebird.de']
+    start_urls = ['https://www.bravebird.de/']
+
+    def parse(self, response):
+        test_filter = response.xpath('//div[@class="contentbox-content"]/h2/text()').extract()
+        for items in zip(test_filter):
+            print(items[0])
